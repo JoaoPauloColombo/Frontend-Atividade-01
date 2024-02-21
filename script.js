b44fb83075744ee3f8287e581e726ffe3cb09697
Ju4uz <joaosouzapaulo446@gmail.com>
2024-02-21 19:31:30 -0300

    1.4

--- script.js
+++ script.js
@@ -7,11 +7,22 @@ botao.addEventListener('click', function(event){
     event.preventDefault();
 
     const pessoa = {
-        longin: login.value,
-        nome: nome.value,
+        name: nome.value,
+        login: login.value,
         categoria: categoria.value,
+        valor: valor.value,
+        NumProdutos:NumProdutos.value,
         lembrete: lembrete.value
+
     }
+
+    nome.value = ""
+    login.value = ""
+    valor.value = ""
+    NumProdutos.value = ""
+    lembrete.value = ""
+
+
     lista.push(pessoa)
     console.log(lista);
 })
