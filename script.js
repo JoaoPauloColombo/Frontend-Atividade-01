e7708c65786c4dcd40e3ed243d96c76cd15e4efc
VictorSantosSilvaa <157403216+VictorSantosSilvaa@users.noreply.github.com>
2024-02-21 16:18:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ botao.addEventListener('click', function(event){
         longin: login.value,
         nome: nome.value,
         categoria: categoria.value,
-        lembrete: lembrete.value,
+        lembrete: lembrete.value
     }
     lista.push(pessoa)
     console.log(lista)
