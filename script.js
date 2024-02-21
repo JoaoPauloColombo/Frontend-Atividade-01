4a58ee53372578062161dc63157dcba20d047f58
VictorSantosSilvaa <157403216+VictorSantosSilvaa@users.noreply.github.com>
2024-02-21 16:03:55 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+/* Victor */
+const botao = document.getElementById("btn");
+
+const lista = []
+
+botao.addEventListener('click', function(event){
+    event.preventDefault();
+
+    const pessoa = {
+        longin: login.value,
+        nome: nome.value,
+        categoria: categoria.value,
+        
+    }
+})
